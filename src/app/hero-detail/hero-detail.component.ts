631c7621d14f5dde0091cfb2e4812fe70abc9e3a
Lapierre Bruno <bruno.lapierre@umontreal.ca>
2019-07-04 11:40:59 -0400

    modif erreur css

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -12,8 +12,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
 
-  /* @Input() hero: Hero; */
-
+  /*  import { Input } from '@angular/core';   @Input() hero: Hero;     */
   hero: Hero;
 
   constructor(
