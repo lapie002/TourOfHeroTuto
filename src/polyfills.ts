7520987d0c1133cbc3d158edbc20d8e3c1a54f36
Lapierre Bruno <bruno.lapierre@umontreal.ca>
2019-07-04 13:33:07 -0400

    added reflect

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,8 +55,13 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+
+/* import 'zone.js/dist/zone';  // Included with Angular CLI. */
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone-patch-canvas';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
